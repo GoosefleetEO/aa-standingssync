212f04537a0702377541fa713c5a706244189a93
ErikKalkoken <erik.kalkoken@gmail.com>
2022-08-04 20:32:02 +0200

    Add integration test with WT

--- standingssync/__init__.py
+++ standingssync/__init__.py
@@ -1,4 +1,4 @@
 default_app_config = "standingssync.apps.SyncAltContactsConfig"
 
-__version__ = "1.5.0b3"
+__version__ = "1.5.0b4"
 __title__ = "Standings Sync"
--- standingssync/tests/test_integration.py
+++ standingssync/tests/test_integration.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 from django.test import override_settings
+from eveuniverse.models import EveEntity
 
 from app_utils.esi_testing import BravadoOperationStub
 from app_utils.testing import NoSocketsTestCase
@@ -8,6 +9,7 @@ from app_utils.testing import NoSocketsTestCase
 from ..tasks import run_manager_sync
 from .factories import (
     EveEntityCharacterFactory,
+    EveWarFactory,
     SyncedCharacterFactory,
     SyncManagerFactory,
 )
@@ -20,8 +22,8 @@ MODELS_PATH = "standingssync.models"
 @patch(MODELS_PATH + ".STANDINGSSYNC_WAR_TARGETS_LABEL_NAME", "WAR TARGETS")
 @patch(MODELS_PATH + ".esi")
 class TestIntegration(NoSocketsTestCase):
-    @patch(MODELS_PATH + ".STANDINGSSYNC_ADD_WAR_TARGETS", True)
-    def test_should_sync_manager_and_characters(self, mock_esi):
+    @patch(MODELS_PATH + ".STANDINGSSYNC_ADD_WAR_TARGETS", False)
+    def test_should_sync_manager_and_character_no_wt(self, mock_esi):
         # given
         manager = SyncManagerFactory()
         sync_character = SyncedCharacterFactory(manager=manager)
@@ -42,9 +44,45 @@ class TestIntegration(NoSocketsTestCase):
         # when
         run_manager_sync.delay(manager_pk=manager.pk)
         # then
-        character_contact_ids = esi_character_contacts._contacts[
+        character_contacts = esi_character_contacts._contacts[
             sync_character.character.character_id
         ]
-        self.assertIn(my_alliance_contact.id, character_contact_ids)
-        self.assertIn(manager.alliance.alliance_id, character_contact_ids)
-        self.assertNotIn(sync_character.character.character_id, character_contact_ids)
+        self.assertEqual(character_contacts[my_alliance_contact.id].standing, 5)
+        self.assertEqual(character_contacts[manager.alliance.alliance_id].standing, 10)
+        self.assertNotIn(
+            sync_character.character.character_id, character_contacts.keys()
+        )
+
+    @patch(MODELS_PATH + ".STANDINGSSYNC_ADD_WAR_TARGETS", True)
+    def test_should_sync_manager_and_character_with_wt(self, mock_esi):
+        # given
+        manager = SyncManagerFactory()
+        alliance = EveEntity.objects.get(id=manager.alliance.alliance_id)
+        war = EveWarFactory(defender=alliance)
+        sync_character = SyncedCharacterFactory(manager=manager)
+        character = EveEntityCharacterFactory(
+            id=sync_character.character.character_id,
+            name=sync_character.character.character_name,
+        )
+        my_alliance_contact = EveEntityCharacterFactory()
+        alliance_contacts = [
+            create_esi_contact(character),
+            create_esi_contact(my_alliance_contact),
+        ]
+        mock_esi.client.Contacts.get_alliances_alliance_id_contacts.return_value = (
+            BravadoOperationStub(alliance_contacts)
+        )
+        esi_character_contacts = EsiCharacterContactsStub()
+        esi_character_contacts.setup_esi_mock(mock_esi)
+        # when
+        run_manager_sync.delay(manager_pk=manager.pk)
+        # then
+        character_contacts = esi_character_contacts._contacts[
+            sync_character.character.character_id
+        ]
+        self.assertEqual(character_contacts[my_alliance_contact.id].standing, 5)
+        self.assertEqual(character_contacts[manager.alliance.alliance_id].standing, 10)
+        self.assertEqual(character_contacts[war.aggressor.id].standing, -10)
+        self.assertNotIn(
+            sync_character.character.character_id, character_contacts.keys()
+        )
