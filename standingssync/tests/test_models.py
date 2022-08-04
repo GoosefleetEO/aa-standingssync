2c5c9d96f896dbd20e878282a832770399cf5b15
ErikKalkoken <erik.kalkoken@gmail.com>
2022-08-04 14:28:34 +0200

    Move EsiContactsStub to utils

--- standingssync/tests/test_models.py
+++ standingssync/tests/test_models.py
@@ -1,6 +1,4 @@
-import copy
 import datetime as dt
-from enum import Enum
 from unittest.mock import Mock, patch
 
 from django.test import TestCase
@@ -25,7 +23,12 @@ from .factories import (
     SyncManagerFactory,
     UserMainSyncerFactory,
 )
-from .utils import ALLIANCE_CONTACTS, LoadTestDataMixin
+from .utils import (
+    ALLIANCE_CONTACTS,
+    EsiCharacterContactsStub,
+    EsiContactStub,
+    LoadTestDataMixin,
+)
 
 MODELS_PATH = "standingssync.models"
 
@@ -429,163 +432,6 @@ class TestSyncManager2(NoSocketsTestCase):
         result = sync_manager.update_from_esi()
         # then
         self.assertTrue(result)
-
-
-class EsiContactStub:
-    class ContactType(Enum):
-        CHARACTER = "character"
-        CORPORATION = "corporation"
-        ALLIANCE = "alliance"
-
-    def __init__(
-        self,
-        contact_id: int,
-        contact_type: str,
-        standing: float,
-        label_ids: list = None,
-    ) -> None:
-        if contact_type not in self.ContactType:
-            raise ValueError(f"Invalid contact_type: {contact_type}")
-
-        self._contact_id = int(contact_id)
-        self._contact_type = contact_type
-        self.standing = float(standing)
-        self.label_ids = list(label_ids) if label_ids else None
-
-    def __repr__(self) -> str:
-        return (
-            "{}("
-            "{}"
-            ", {}.{}"
-            ", standing={}"
-            "{}"
-            ")".format(
-                type(self).__name__,
-                self.contact_id,
-                type(self).__name__,
-                self.contact_type,
-                self.standing,
-                f", label_ids={self.label_ids}" if self.label_ids else "",
-            )
-        )
-
-    def __key(self):
-        return (
-            self.contact_id,
-            self.contact_type,
-            self.standing,
-            tuple(self.label_ids) if self.label_ids else None,
-        )
-
-    def __hash__(self):
-        return hash(self.__key())
-
-    def __eq__(self, other):
-        if isinstance(other, type(self)):
-            return self.__key() == other.__key()
-        return NotImplemented
-
-    @property
-    def contact_id(self):
-        return self._contact_id
-
-    @property
-    def contact_type(self):
-        return self._contact_type
-
-    def to_esi_dict(self) -> dict:
-        return {
-            "contact_id": self.contact_id,
-            "contact_type": self.ContactType(self.contact_type),
-            "standing": self.standing,
-            "label_ids": self.label_ids,
-        }
-
-
-class EsiCharacterContactsStub:
-    """Simulates the contacts for a character on ESI"""
-
-    def __init__(self) -> None:
-        self._contacts = dict()
-        self._labels = dict()
-
-    def setup_contacts(self, character_id: int, contacts: list):
-        self._contacts[character_id] = dict()
-        if character_id not in self._labels:
-            self._labels[character_id] = dict()
-        for contact in contacts:
-            if contact.label_ids:
-                for label_id in contact.label_ids:
-                    if label_id not in self.labels(character_id).keys():
-                        raise ValueError(f"Invalid label_id: {label_id}")
-            self._contacts[character_id][contact.contact_id] = copy.deepcopy(contact)
-
-    def setup_labels(self, character_id: int, labels: dict):
-        self._labels[character_id] = dict(labels)
-
-    def contacts(self, character_id: int) -> dict:
-        return self._contacts[character_id].values()
-
-    def labels(self, character_id: int) -> dict:
-        return self._labels[character_id] if character_id in self._labels else dict()
-
-    def esi_get_characters_character_id_contacts(self, character_id, token, page=None):
-        contacts = [obj.to_esi_dict() for obj in self._contacts[character_id].values()]
-        return BravadoOperationStub(contacts)
-
-    def esi_get_characters_character_id_contacts_labels(
-        self, character_id, token, page=None
-    ):
-        labels = [
-            {"label_id": k, "label_name": v}
-            for k, v in self._labels[character_id].items()
-        ]
-        return BravadoOperationStub(labels)
-
-    def esi_post_characters_character_id_contacts(
-        self, character_id, contact_ids, standing, token, label_ids=None
-    ):
-        self._check_label_ids_valid(character_id, label_ids)
-        contact_type_map = {
-            1: EsiContactStub.ContactType.CHARACTER,
-            2: EsiContactStub.ContactType.CORPORATION,
-            3: EsiContactStub.ContactType.ALLIANCE,
-        }
-        for contact_id in contact_ids:
-            contact_type = contact_type_map[contact_id // 1000]
-            self._contacts[character_id][contact_id] = EsiContactStub(
-                contact_id=contact_id,
-                contact_type=contact_type,
-                standing=standing,
-                label_ids=label_ids,
-            )
-        return BravadoOperationStub([])
-
-    def esi_put_characters_character_id_contacts(
-        self, character_id, contact_ids, standing, token, label_ids=None
-    ):
-        self._check_label_ids_valid(character_id, label_ids)
-        for contact_id in contact_ids:
-            self._contacts[character_id][contact_id].standing = standing
-            if label_ids:
-                if not self._contacts[character_id][contact_id].label_ids:
-                    self._contacts[character_id][contact_id].label_ids = label_ids
-                else:
-                    self._contacts[character_id][contact_id].label_ids += label_ids
-        return BravadoOperationStub([])
-
-    def esi_delete_characters_character_id_contacts(
-        self, character_id, contact_ids, token
-    ):
-        for contact_id in contact_ids:
-            del self._contacts[character_id][contact_id]
-        return BravadoOperationStub([])
-
-    def _check_label_ids_valid(self, character_id, label_ids):
-        if label_ids:
-            for label_id in label_ids:
-                if label_id not in self.labels(character_id).keys():
-                    raise ValueError(f"Invalid label_id: {label_id}")
 
 
 @patch(MODELS_PATH + ".STANDINGSSYNC_WAR_TARGETS_LABEL_NAME", "WAR TARGETS")
